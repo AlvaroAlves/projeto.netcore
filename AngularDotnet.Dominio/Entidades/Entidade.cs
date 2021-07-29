a585882f837aeda10b5d8e22dec394f892cf2de6
alvaro alves <alvaroalves87@gmail.com>
2021-07-29 12:21:04 -0300

    Adição das primeiras classes

---
+++ AngularDotnet.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngularDotnet.Dominio.Entidades
+{
+    public abstract class Entidade
+    {
+        
+    }
+}
---
+++ AngularDotnet.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngularDotnet.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        /// <summary>
+        /// Um pedido pode ter um ou muitos itens
+        /// </summary>
+        public ICollection<ItemPedido> ItemPedidos { get; set; }
+    }
+}
