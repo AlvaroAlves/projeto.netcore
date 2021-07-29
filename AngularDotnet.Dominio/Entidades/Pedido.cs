5fae6ae2ddc7906bebec82328cd9caf493228100
alvaro alves <alvaroalves87@gmail.com>
2021-07-29 12:32:12 -0300

    Criação das entidades pedido e enums de forma de pagamento

--- AngularDotnet.Dominio/Entidades/Pedido.cs
+++ AngularDotnet.Dominio/Entidades/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AngularDotnet.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,15 @@ namespace AngularDotnet.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string Cep { get; set; }
+        public string Estado{ get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
         /// <summary>
         /// Um pedido pode ter um ou muitos itens
         /// </summary>
---
+++ AngularDotnet.Dominio/Enums/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngularDotnet.Dominio.Enums
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido,
+        Boleto,
+        CartaoCredito,
+        Deposito
+    }
+}
