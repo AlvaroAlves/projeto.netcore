a3957946e684e913729b8be8376ee2ccdd00085b
alvaro alves <alvaroalves87@gmail.com>
2021-07-29 16:55:27 -0300

    Adicionado classe de contexto

---
+++ AngularDotnet.Repositorio/Contexto/AngularDotnetContexto.cs
@@ -0,0 +1,18 @@
+﻿using AngularDotnet.Dominio.Entidades;
+using AngularDotnet.Dominio.ObjetoDeValor;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngularDotnet.Repositorio.Contexto
+{
+    public class AngularDotnetContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+    }
+}
