fbf6d5803886b53d8175eba19ddda33f83fe62f1
alvaro alves <alvaroalves87@gmail.com>
2021-07-30 16:56:39 -0300

    Criação do modelo angular do Usuario, ajustes e validações básicas na tela de login

--- AngularDotnet.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ AngularDotnet.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,9 +1,18 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../model/usuario";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-  
+  public usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    alert(this.usuario.email + " " + this.usuario.senha);
+  }
 }
