5ea1403bc1389779949c9730ca1983bc10bfdaf2
alvaro alves <alvaroalves87@gmail.com>
2021-08-02 10:15:46 -0300

    Criado serviço de usuário no angular

--- AngularDotnet.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ AngularDotnet.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../model/usuario";
 import { ActivatedRoute, Router } from "@angular/router";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -11,7 +12,8 @@ export class LoginComponent implements OnInit {
   public usuario;
   public returnUrl: string;
 
-  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute,
+    private usuarioServico: UsuarioServico) {
   }
     ngOnInit(): void {
       this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
@@ -19,9 +21,18 @@ export class LoginComponent implements OnInit {
     }
 
   entrar() {
-    if (this.usuario.email == "email@teste.com" && this.usuario.senha == "abc123") {
+    this.usuarioServico.verificaUsuario(this.usuario)
+      .subscribe(
+        data => {
+
+        },
+        err => {
+
+        }
+      );
+    /*if (this.usuario.email == "email@teste.com" && this.usuario.senha == "abc123") {
       sessionStorage.setItem("usuario-autenticado", "1");
       this.router.navigate([this.returnUrl]);
-    }
+    }*/
   }
 }
