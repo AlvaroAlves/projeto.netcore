8f9a3a20e84178893f468b5a1c3a2834f9d8478f
alvaro alves <alvaroalves87@gmail.com>
2021-08-14 14:57:05 -0300

    Ajuste no serviço de produto

--- AngularDotnet.Web/ClientApp/src/app/servicos/produto/produto.servico.ts
+++ AngularDotnet.Web/ClientApp/src/app/servicos/produto/produto.servico.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, inject } from "@angular/core";
+import { Injectable, Inject, inject, OnInit } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Produto } from "../../model/produto"
@@ -6,10 +6,15 @@ import { Produto } from "../../model/produto"
 @Injectable({
   providedIn: "root"
 })
-export class ProdutoServico {
+export class ProdutoServico implements OnInit {
   private _baseUrl: string;
+  public produtos: Produto[];
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this._baseUrl = baseUrl;
+  }
+
+  ngOnInit(): void {
+    this.produtos = [];
   }
 
   get headers(): HttpHeaders {
@@ -25,5 +30,13 @@ export class ProdutoServico {
 
   public deletar(produto: Produto): Observable<Produto> {
     return this.http.post<Produto>(this._baseUrl + "api/produto/deletar", JSON.stringify(produto), { headers: this.headers });
+  }
+
+  public obterTodosOsProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(this._baseUrl + "api/produto");
+  }
+
+  public obterProduto(produtoId: number): Observable<Produto> {
+    return this.http.get<Produto>(this._baseUrl + "api/produto/obter");
   }
 }
